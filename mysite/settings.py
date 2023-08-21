a39ead81b4af9b55063ac91230a6015e5865003a
rabciak <rabciak@gmail.com>
2023-08-21 17:41:38 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,8 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-POETRY_VERSION = '1.6.0'
-PYTHON_VERSION = '3.9.17'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
