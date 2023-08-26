8dc8f91a01432f9b5a47d819a210d26d446b3f67
rabciak <rabciak@gmail.com>
2023-08-26 20:21:36 +0200

    Revert "layercontrol"
    This reverts commit 21e888c93b9623215a4385027440f338db219768.

--- render/views.py
+++ render/views.py
@@ -1,18 +1,13 @@
 import json
 from django.core.serializers import serialize
 from django.views.generic import TemplateView
-from render.models import NuclearReactor, WorldBorder
+from render.models import NuclearReactor
 
 class MarkersMapView(TemplateView):
     template_name = "render//index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
         markers = NuclearReactor.objects.all()
         context["markers"] = json.loads(serialize("geojson", markers))
-        
-        world_border = WorldBorder.objects.all()
-        context["world_border"] = json.loads(serialize("geojson", world_border))
-        
         return context
