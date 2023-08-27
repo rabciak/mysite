35f66dcc82008d825e8395c46185f6c56b50624a
rabciak <rabciak@gmail.com>
2023-08-27 21:34:42 +0200

    Update map.js

--- render/static/map.js
+++ render/static/map.js
@@ -7,6 +7,22 @@ const features = L.geoJSON(markers);
 const worldCenter = [20, 0]; // Center coordinates
 const worldZoom = 3; // Zoom level for world view
 
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+});
+
+var baseMaps = {
+    "Base Layer": osm,
+    "google sat": googleSat // Replace 'baseLayer' with your actual base layer
+};
+
+var overlayMaps = {
+    "Generators": features
+};
+
+var layerControl = L.control.layers(baseMaps,overlayMaps).addTo(map);
+
 // Set the initial view of the map
 map.setView(worldCenter, worldZoom);
 
