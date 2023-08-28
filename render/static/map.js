120233b5a9bf570dc1895bb58f8a13fa9aa7ee35
rabciak <rabciak@gmail.com>
2023-08-28 23:36:29 +0200

    custom icon

--- render/static/map.js
+++ render/static/map.js
@@ -3,7 +3,15 @@ const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
 const map = L.map("map", { layers: [osm] });
 const markers = JSON.parse(document.getElementById("markers-data").textContent);
-const features = L.geoJSON(markers,{
+//Make features variable with popup of properties when clicked.
+
+var MyIcon = L.icon({
+  iconUrl: 'https://i.ibb.co/qnJV9tM/thenounproject.png',
+  iconSize:     [75, 75], // size of the icon
+});
+
+const features = L.geoJSON(markers,  
+  { 
     onEachFeature: (feature, layer) => {
       const popupContent = `
       <b>Name:</b> ${feature.properties.name_en}<br>
@@ -12,26 +20,32 @@ const features = L.geoJSON(markers,{
     `; 
   
       layer.bindPopup(popupContent);
-    }
+    },
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { 
+      icon: MyIcon,
+      riseOnHover: true });
+  }
   });
+//variables to make the correct view
 const worldCenter = [20, 0]; // Center coordinates
 const worldZoom = 3; // Zoom level for world view
-
-googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+//add different basemap
+const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
 });
-
-var baseMaps = {
+//define basemap list
+const baseMaps = {
     "Base Layer": osm,
     "google sat": googleSat // Replace 'baseLayer' with your actual base layer
 };
-
-var overlayMaps = {
+//define layers list
+const overlayMaps = {
     "Generators": features
 };
-
-var layerControl = L.control.layers(baseMaps,overlayMaps).addTo(map);
+//add layer control to the map
+const layerControl = L.control.layers(baseMaps,overlayMaps).addTo(map);
 
 // Set the initial view of the map
 map.setView(worldCenter, worldZoom);
